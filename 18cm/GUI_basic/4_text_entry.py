cdb033bbfd4b3fa127ee828c37bcaff7183c8957
woojin20212321 <gksdnwls391@naver.com>
2021-10-13 23:33:32 +0900

    파이썬 tkinter

--- 18cm/GUI_basic/4_text_entry.py
+++ 18cm/GUI_basic/4_text_entry.py
@@ -13,4 +13,14 @@ e = Entry(root, width=30)
 e.pack()
 e.insert(0, "한 줄만 입력해요")
 
+def btncmd():
+    print(txt.get("1.0", END))
+    print(e.get())
+
+txt.delete("1.0", END) # 1 : 첫번쨰 라인, 0 번째 column 위치
+e.delete(0, END)
+
+btn = Button(root, text="입력", command=btncmd)
+btn.pack()
+
 root.mainloop()
