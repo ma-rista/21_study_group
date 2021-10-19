94acc67cf0e9c3f0965dfdb4d351d509ce7041c6
woojin20212321 <gksdnwls391@naver.com>
2021-10-20 02:03:48 +0900

    메모장 완성

--- 18cm/GUI_basic/15_메모장.py
+++ 18cm/GUI_basic/15_메모장.py
@@ -1,49 +1,58 @@
+import os
 from tkinter import *
 
-
 root = Tk()
-root.title("뭬모쥉")
+root.title("제목 없음")
 root.geometry("640x480")
 
+# 열기, 저장 파일 이름
+filename = "myno te.txt"
 
-frame = Frame(root)
-frame.pack()
+def open_file():
+    if os.path.isfile(filename): # 파일 있으면 True, 없으면 False
+        with open(filename, "r", encoding="utf8") as file:
+            txt.delete("1.0", END) # 텍스트 위젯 본문 삭제
+            txt.insert(END, file.read()) # 파일 내용을 본문에 입력
 
-
-# 스크롤
-scrollbar = Scrollbar()
-scrollbar.pack(side="right", fill="y")
-
-# 텍스트 입력 창
-txt = Text(root, width=640, height=480, yscrollcommand=scrollbar.set)
-txt.pack(side="left")
-
+def save_file():
+    with open(filename, "w", encoding="utf8") as file:
+        file.write(txt.get("1.0", END)) # 모든 내용을 가져와서 저장
 
 # 상단 메뉴 바
 menu = Menu(root)
+ 
 
 # 파일 탭 (상단 메뉴 바)
 menu_file = Menu(menu, tearoff=0)
-menu.add_cascade(label="파일", menu=menu_file)
+menu_file.add_command(label="열기", command=open_file)
+menu_file.add_command(label="저장", command=save_file)
 menu_file.add_separator()
-menu_file.add_command(label="열기")
-menu_file.add_command(label="저장")
 menu_file.add_command(label="끝내기", command=root.quit)
+menu.add_cascade(label="파일", menu=menu_file)
 
-
-# 편집 탭 (상단 메뉴 바)
+# ㅍ편집, 서식, 보기, 도움말
 menu_edit = Menu(menu, tearoff=0)
 menu.add_cascade(label="편집", menu=menu_edit)
-
-# 서식 탭 (상단 메뉴바)
 menu_ser = Menu(menu, tearoff=0)
 menu.add_cascade(label="서식", menu=menu_ser)
+menu_see = Menu(menu, tearoff=0)
+menu.add_cascade(label="보기", menu=menu_see)
+menu_help = Menu(menu, tearoff=0)
+menu.add_cascade(label="도움말", menu=menu_help)
+
+# 스크롤바
+scrollbar = Scrollbar(root)
+scrollbar.pack(side="right", fill="y")
+
+# 텍스트 입력 창
+txt = Text(root, yscrollcommand=scrollbar.set)
+txt.pack(side="left", fill="both", expand=True)
 
 scrollbar.config(command=txt.yview)
 root.config(menu=menu)
 root.mainloop()
 
-
+ 
 
 
 
