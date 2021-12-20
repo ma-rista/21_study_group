e9e11aacd9425c2fec8568aa3c3a63f2ca2d2c1d
woojin20212321 <gksdnwls391@naver.com>
2021-12-20 17:29:29 +0900

    html

--- 18cm/웹스크래핑/web_scrapper.py
+++ 18cm/웹스크래핑/web_scrapper.py
@@ -3,10 +3,15 @@ from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://kr.indeed.com/jobs?q=python&l&vjk=c25feb431668b850")
 
-print(indeed_result.text)
+
  
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 pagination = indeed_soup.find("div", {"class": "pagination"})
 
-print(pagination)
+pages = pagination.find_all('a')
+spans = []
+for page in pages:
+    spans.append(page.find("span"))  
+print(spans[0:-1])
+
